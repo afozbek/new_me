635072873f6d53eb2f510e5c44b8255c022f8ab6
Abdullah Furkan Özbek <furkanozbek1995@gmail.com>
2024-12-18 14:03:04 +0300

    updated necessary configs and links

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Abdullah Furkan Özbek";
+export const SITE_DESCRIPTION = "Welcome to my world!";
